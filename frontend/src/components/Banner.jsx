8504c2a3d113577fa015219724d42c7b60c56f53
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-08-14 00:56:10 +0530

    Update Banner.jsx

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -109,7 +109,7 @@ const Banner = () => {
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        src="/logo2.jpeg"
+        src="/log1.jpeg"
         alt="bannerImg"
       />
       <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
